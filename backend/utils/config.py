0ff85848911332797c13d6cafd33b87dd9e80883
ipetrom <ipeetro1@gmail.com>
2025-10-24 19:39:34 +0200

    bug fixed 🧚

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -11,11 +11,10 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="../.env", env_file_encoding='utf-8', extra='ignore')
 
     # Klucze API
-    OPENAI_API_KEY: str = "brak_klucza_openai"
     SCALEWAY_API_KEY: str = "a3c535e8-93ba-46bf-847e-7115853dcc0e"
 
     # URL dla API Scaleway (możesz je tu trzymać)
-    SCALEWAY_API_URL: str = "https://api.scaleway.ai/f515e043-422b-48bf-95a7-2da7063d7858/v1"
+    SCALEWAY_API_URL: str = "https://api.scaleway.ai/f515e043-422b-48bf-95a7-2da7063d7858/v1/chat/completions"
 
 # Tworzymy jedną, globalną instancję ustawień,
 # którą inne moduły będą mogły importować.
